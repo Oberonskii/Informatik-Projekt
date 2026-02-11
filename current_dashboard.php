baa10de2ccd6dde56e46d5d18440c0f1392f1b91
Oberonskii <80541525+Oberonskii@users.noreply.github.com>
2026-02-11 13:02:25 +0100

    Add session check for user authentication

--- current_dashboard.php
+++ current_dashboard.php
@@ -1,3 +1,15 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+?>
+
+
 <!DOCTYPE html>
 <html lang="de">
 <head>
